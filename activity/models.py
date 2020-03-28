30b4c2e5aa9a446e6fb7cf764b643351d17b381a
prkarjgi <pkarajgikar@gmail.com>
2020-03-28 15:10:34 +0530

    Initial commit.

---
+++ activity/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+# class ActivityPeriod(models.Model):
+#     user_id = models.ForeignKey('', on_delete=models.CASCADE)
+#     start_time = models.DateTimeField()
+#     end_time = models.DateTimeField()
