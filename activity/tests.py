0786cfa71c8e0ea5afb379526ebbe0e2907a8899
prkarjgi <pkarajgikar@gmail.com>
2020-03-29 13:36:54 +0530

    Added test cases for creating user

--- activity/tests.py
+++ activity/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from activity.models import MyUser, ActivityPeriod
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        user1 = MyUser.objects.create_user(
+            user_id='Q100',
+            first_name='Test',
+            last_name='Case',
+            tz='America/Los_Angeles',
+            password='testing'
+        )
+        user2 = MyUser.objects.create_user(
+            user_id='Q101',
+            first_name='Fake',
+            last_name='Name',
+            tz='Asia/Kolkata',
+            password='production'
+        )
+        user1.save()
+        user2.save()
+
+    def test_create_user(self):
+        users = MyUser.objects.count()
+        self.assertEqual(users, 2)
